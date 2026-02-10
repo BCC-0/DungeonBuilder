2e9564cc9d1be540befd6a26ab33c42948da3548
BCC-0 <Bram.Clarenbeek@gmail.com>
2026-02-10 10:22:27 +0100

    Started on adding the items structure.
    Added a base item scriptable object class and three specific item classes: weapon, tool, consumable item.
    Using this, we can simply create new items by inheriting from the specific subclass and filling in the scriptable object variables in the unity editor.

---
+++ Assets/Scripts/Items/ConsumableItem.cs
@@ -0,0 +1,12 @@
+﻿
+/// <summary>
+/// 
+/// </summary>
+public abstract class ConsumableItem : Item
+{
+    // Optional shared properties for all consumables can go here
+    // e.g., public int maxStackSize = 1;
+
+    // Subclasses have to implement their own use behavior
+    public abstract override void Use();
+}
